b1a7b728e76f47a984b4a355f4dd16d80b9e2277
Jasmine Kaur Wadhwa <jwadhwa@ualberta.ca>
2022-09-12 19:08:13 -0600

    Update get.py

--- get.py
+++ get.py
@@ -4,4 +4,4 @@ print(requests.__version__)
 
 print(requests.get("https://www.google.com"))
 
-print(requests.get("https://raw.githubusercontent.com/jwadhwa213/Cmput404/main/get.py"))
+print(requests.get("https://raw.githubusercontent.com/jwadhwa213/Cmput404/main/get.py").text)
